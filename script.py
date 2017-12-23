309ed0ec7f88752c72e1f3daf0a2567c8bfea36b
prince2398 <princepiyush2398@gmail.com>
2017-12-24 00:15:52 +0530

    thug life mask pasted successfully

--- script.py
+++ script.py
@@ -1,33 +1,25 @@
-import numpy as np 
 import cv2
 from PIL import Image
-import pprint
+import sys
 
-haarcascade_path = "//home//prince//opencv-tmp//opencv-3//data//haarcascades//"
 face_cascade_file = "haarcascade_frontalface_default.xml"
-eye_cascade_file = "haarcascade_eye.xml"
-image_path = input("Enter path to image or only the file name if fileis in same directory : ")
+image_path = sys.argv[1]
+mask_path = "mask.png"
 
-
-face_cascade = cv2.CascadeClassifier(haarcascade_path + face_cascade_file)
-eye_cascade = cv2.CascadeClassifier(haarcascade_path + eye_cascade_file)
+face_cascade = cv2.CascadeClassifier(face_cascade_file)
 
 
 img = cv2.imread(image_path , 1)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-# 	pass
-# else :
-# 	print("Error 404 : No Image Found ")
-# 	exit()
+image = Image.open(image_path)
 
+faces = face_cascade.detectMultiScale(gray,1.15)
 
-faces = face_cascade.detectMultiScale(gray,1.3,5)
 
 for (x,y,w,h) in faces:
-	roi_gray = gray[y:y+h , x:x+w]
-	eyes = eye_cascade.detectMultiScale(roi_gray)
-	print(eyes)
-
-print(faces)
-
-
+	box = (x,y)
+	mask = Image.open(mask_path)
+	mask = mask.resize((w,h))
+	image.paste(mask, box , mask = mask)
+	
+image.save("thug.png")
